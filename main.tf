b5a96ae00b1b34a1924f58d2ba7fe1de5f08f6a8
streaming <streaming@shodan.local>
2022-04-30 17:43:01 +0400

    fix connection issues

--- main.tf
+++ main.tf
@@ -12,28 +12,59 @@ terraform {
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
+
   default_tags {
-      tags = {
-          Source = "Terraform"
-      }
+    tags = {
+      Source = "Terraform"
+    }
   }
 }
 
 resource "aws_vpc" "secure_cloud" {
-    cidr_block = "11.0.0.0/16"
-    enable_dns_hostnames = true
+  cidr_block           = "11.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    "Name" = "Secure cloud"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.secure_cloud.id
+
+  tags = {
+    "Name" = "DMZ gateway"
+  }
+}
+
+resource "aws_route_table" "routing_table" {
+  vpc_id = aws_vpc.secure_cloud.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
 }
 
 resource "aws_subnet" "public" {
-    vpc_id = aws_vpc.secure_cloud.id
-    cidr_block = "11.0.1.0/24"
-    map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.secure_cloud.id
+  cidr_block              = "11.0.0.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name" = "DMZ"
+  }
+}
+
+resource "aws_route_table_association" "internet_access" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.routing_table.id
 }
 
 resource "aws_security_group" "bitwarden_sg" {
   vpc_id = aws_vpc.secure_cloud.id
   ingress {
-    description = "TLS from everywhere"
+    description      = "TLS from everywhere"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
@@ -41,7 +72,7 @@ resource "aws_security_group" "bitwarden_sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
   ingress {
-    description = "SSH from everywhere"
+    description      = "SSH from everywhere"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
@@ -58,12 +89,15 @@ resource "aws_security_group" "bitwarden_sg" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0f9fc25dd2506cf6d"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public.id
+  ami                    = "ami-0f9fc25dd2506cf6d"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.bitwarden_sg.id]
+  depends_on = [
+    aws_internet_gateway.gw
+  ]
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "BitWarden"
   }
 }
